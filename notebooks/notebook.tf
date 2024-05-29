d7aabc1bc52b67fc9ae86f4997b015fba2b4ab62
Thanh Luu <congthanhluu@gmail.com>
2024-05-29 17:06:12 +0700

    fixing pipeline failures

--- notebooks/notebook.tf
+++ notebooks/notebook.tf
@@ -23,33 +23,14 @@ resource "google_workbench_instance" "instance" {
     location = var.zone
 
     gce_setup {
-        machine_type = var.machine_type // cant be e2 because of accelerator
-        # accelerator_configs {
-        #     type         = var.accelerator_type
-        #     core_count   = var.accelerator_core_count
-        # }
-
-    # shielded_instance_config {
-    #   enable_secure_boot = true
-    #   enable_vtpm = true
-    #   enable_integrity_monitoring = true
-    # }
+        machine_type = var.machine_type
 
         disable_public_ip = false
-
-        # service_accounts {
-        #   email = var.service_accounts
-        # }
 
         boot_disk {
           disk_size_gb  = var.boot_disk_size_gb
           disk_type = var.boot_disk_type
         }
-
-        # data_disks {
-        #   disk_size_gb  = var.data_disk_size_gb
-        #   disk_type = var.data_disk_type
-        # }
 
         network_interfaces {
           network = data.google_compute_network.workbench_network.id
@@ -63,17 +44,12 @@ resource "google_workbench_instance" "instance" {
 
         enable_ip_forwarding = false
 
-        # tags = ["abc", "def"]
 
     }
 
     disable_proxy_access = "false"
 
     instance_owners  = [var.instance_owner]
-
-    labels = {
-        k = "val"
-    }
 
     desired_state = "ACTIVE"
 
